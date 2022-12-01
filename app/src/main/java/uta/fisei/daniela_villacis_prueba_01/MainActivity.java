b3e70ab418e0edb68b5079162c22f58a9ec6856b
DanielaVillacis <dvillacis17@hotmail.com>
2022-12-01 17:48:40 -0500

    COMMIT 17:48

--- app/src/main/java/uta/fisei/daniela_villacis_prueba_01/MainActivity.java
+++ app/src/main/java/uta/fisei/daniela_villacis_prueba_01/MainActivity.java
@@ -3,12 +3,16 @@ package uta.fisei.daniela_villacis_prueba_01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private Editable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
